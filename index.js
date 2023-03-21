d5410037051e4083ea266beae589a24993ef6b8a
E. F. Jumi Strandberg <96219257+jumistrandberg@users.noreply.github.com>
2023-03-21 20:02:58 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+//Appear animation for Starters h3
+const appear = document.querySelector('.appear');
+const cb = function(entries){
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('inview');
+        }else{
+            entry.target.classList.remove('inview');
+        }
+    });
+}
+const io = new IntersectionObserver(cb);
+io.observe(appear);
+
+let 
