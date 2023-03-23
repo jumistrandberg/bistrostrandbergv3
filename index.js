d558ccc730a263cf3348e5cadaf200df7e364b8c
E. F. Jumi Strandberg <96219257+jumistrandberg@users.noreply.github.com>
2023-03-23 07:42:14 +0100

    Added client-side validation in index.js

--- index.js
+++ index.js
@@ -9,7 +9,29 @@ const cb = function(entries){
         }
     });
 }
-const io = new IntersectionObserver(cb);
-io.observe(appear);
 
-let 
+// Contact Form Client-side Validation
+// Get the form element
+const form = document.getElementById('contact-form');
+
+// Event listner to form's submit button
+form.addEventListener('submit', function(event) {
+    // Prevent form from submitting automatically
+    event.preventDefault();
+
+    // Check if required fields have been filled
+    const name = document.getElementById('Namn').value;
+    const email = document.getElementById('Email').value;
+    const tel = document.getElementById('Tel').value;
+    const subject = document.getElementById('Tel').value;
+    const message = document.getElementById('Meddelande').value;
+
+    if (name === '' || email === '' || tel === '' || subject === '' || message === '') {
+        // If any of the fields are empty, display:
+        const status = document.querySelector(.'status');
+        status.innerHTML = 'Vänligen fyll i alla uppgifter';
+    } else {
+        // If all fields have been filled out, submit
+        form.submit();
+    }
+});
